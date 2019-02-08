7c2fe224c977a3505b6b40577408866cd5e0e7f5
lilu <675221350@qq.com>
2019-02-08 13:27:13 +0800

    Add files via upload

---
+++ precache-manifest.9b7b81766cb41faaf3804637a6a8997e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1be8eb0ab124102984e25b2d02d284af",
+    "url": "/static/media/mybook.1be8eb0a.jpg"
+  },
+  {
+    "revision": "3cedb2f65fbbdb43bdd83a692f633153",
+    "url": "/static/media/head.3cedb2f6.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cdd60d96abaa2b2bc724",
+    "url": "/static/js/main.cdd60d96.chunk.js"
+  },
+  {
+    "revision": "05865a73fa456d8a72c4",
+    "url": "/static/js/1.05865a73.chunk.js"
+  },
+  {
+    "revision": "cdd60d96abaa2b2bc724",
+    "url": "/static/css/main.4a8bfe46.chunk.css"
+  },
+  {
+    "revision": "05865a73fa456d8a72c4",
+    "url": "/static/css/1.b3e33e9b.chunk.css"
+  },
+  {
+    "revision": "f2ec82bfa70781bbbd133051e5db36e6",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3ac5a3ece0c4fae9ea150ac86ac07eba.js"
+  "/precache-manifest.9b7b81766cb41faaf3804637a6a8997e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
